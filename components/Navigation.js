bf94a219bd5815c2c4abb14bbcab467b7b7d84fc
mstilde <matiasemanuelbuttari@hotmail.com>
2025-03-11 12:28:08 -0300

    first commit

---
+++ components/Navigation.js
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+
+import { authContext } from "@/lib/store/auth-context";
+
+import { ImStatsBars } from "react-icons/im";
+
+function Nav() {
+
+    const {user, loading, logout} = useContext(authContext);
+
+    return (  <header className="container max-w-2xl px-6 py-6 mx-auto">
+        <div className="flex items-center justify-between">
+          {user && !loading && (
+          <div className="flex items-center gap-2">
+            <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+              <img
+              className="object-cover w-full h-full" 
+              src={user.photoURL}
+              alt={user.displayName}
+              referrerPolicy="no-referrer"
+              />
+            </div>
+            <small>Buenas {user.displayName}</small>
+          </div>
+          )}
+
+          {user && !loading && (
+            <nav className="flex items-center gap-4">
+              <div>
+                <a href="#stats">
+                  < ImStatsBars className="text-2xl"/>
+                </a>
+              </div>
+              <div>
+                <button onClick={logout} className="btn btn-danger">Salir</button>
+              </div>
+            </nav>
+          )}
+
+        </div>
+      </header>
+      )
+}
+
+export default Nav;
---
+++ components/modals/AddExpensesModal.js
@@ -0,0 +1,160 @@
+import { useContext, useState, useRef } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+
+import Modal from "../Modal";
+import { financeContext } from "@/lib/store/finance-context";
+import { toast } from "react-toastify";
+
+function AddExpensesModal({ show, onClose }) {
+    const [ expenseAmount, setExpenseAmount ] = useState("");
+    const [ selectedCategory, setSelectedCategory ] = useState(null);
+    const [ showAddExpense, setShowAddExpense ] = useState(false);
+    const { expenses, addExpenseItem, addCategory } = useContext(financeContext);
+
+    const titleRef = useRef()
+    const colorRef = useRef()
+
+    const addExpenseItemHandler = async () => {
+
+        const expense = expenses.find(e => {
+            return e.id === selectedCategory
+        }) 
+
+        const newExpense = {
+            color: expense.color,
+            title: expense.title,
+            total: (expense.total || 0) + +expenseAmount,
+            items: [
+                ...expense.items,
+                {
+                    amount: +expenseAmount,
+                    createdAt: new Date(),
+                    id: uuidv4(),
+                },
+            ],
+        };
+
+        try {
+            await addExpenseItem(selectedCategory, newExpense);
+
+            console.log(newExpense);
+            setExpenseAmount("");
+            setSelectedCategory(null);
+            onClose();
+            toast.success("Egreso creado con exito");
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+
+    }
+
+    const addCategoryHandler = async () => {
+        const title = titleRef.current.value.trim();
+        const color = colorRef.current.value;
+        if (!title || !color) {
+            alert("Por favor ingresa un nombre y un color para la categoría.");
+            return;
+        }
+    
+        try {
+            await addCategory({ title, color, total: 0 });
+            setShowAddExpense(false);
+            toast.success("Categoria creada con exito");
+            titleRef.current.value = "";
+            colorRef.current.value = "";
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    };
+    
+    return (
+        <Modal show={show} onClose={onClose}>
+            <div className="flex flex-col gap-4">    
+                <label>Ingresa una cantidad..</label>
+                <input
+                    type="number"
+                    min={0.01}
+                    step={0.01}
+                    placeholder="Enter expense amount"
+                    value={expenseAmount}
+                    onChange={(e) => {
+                        setExpenseAmount(e.target.value)
+                    }}
+                    
+                />
+            </div>
+            {expenseAmount > 0 && (
+                <div className="flex flex-col gap-4 mt-6">
+                    <div className="flex items-center justify-between">
+                        <h3 className="text-2xl capitalize">Selecciona la categoría del egreso</h3>
+                        <button
+                            onClick={() => {
+                                setShowAddExpense(true)
+                            }}
+                            className="text-lime-400">+ Nueva Categoría</button>
+                    </div>
+
+                    {showAddExpense && (
+                    <div className="flex items-center justify-between">
+                        <input
+                            type="text"
+                            placeholder="Nombre de la categoría"
+                            ref={titleRef}
+                        />
+
+                        <label>Elige un color</label>
+                        <input
+                         type="color"
+                         className="w-24 h-10"
+                         ref={colorRef}
+                        />
+
+                        <button onClick={addCategoryHandler} className="btn btn-primary-outline">Crear</button>
+                        <button onClick={() => {
+                            setShowAddExpense(false)
+                        }} className="btn btn-danger">Cancelar</button>
+                    </div>
+                    )}
+                    {expenses.map((expense) => {
+                        return (
+                        <button 
+                            key={expense.id}
+                            onClick={() => {
+                                setSelectedCategory(expense.id);
+                        }}>
+                            <div style={{
+                                boxShadow: expense.id === selectedCategory ? "1px 1px 4px" : "none",
+                            }}
+                            className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+                                <div className="flex items-center gap-2">
+                                    <div className="w-[25px] h-[25px] rounded-full"
+                                    style={{
+                                        backgroundColor: expense.color,
+                                    }}
+                                    />
+                                    <h4 className="capitalize">{expense.title}</h4>
+                                </div>
+                            </div>
+                        </button>
+                        );
+                    })}
+                </div>
+            )}
+            
+            {expenseAmount > 0 && selectedCategory && (
+                <div className="mt-6">
+                    <button
+                    className="btn btn-primary"
+                    onClick={addExpenseItemHandler}>
+                        Añadir gasto
+                    </button>
+                </div>
+            )}
+        </Modal>
+    );
+}
+
+export default AddExpensesModal;
